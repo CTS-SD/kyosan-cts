ba8fe0eb24d17abb1c8ac69a44e429cd4bcb5151
Kazuma Ito <zarigani2x@gmail.com>
2024-09-30 05:23:30 +0900

    update: remove back button on startPage

--- src/app/test/StartPage.tsx
+++ src/app/test/StartPage.tsx
@@ -10,21 +10,15 @@ type Props = {
 
 const StartPage = ({ onStart }: Props) => {
   return (
-    <>
-      <Link href="/" className="flex items-center">
-        <ChevronLeftIcon />
-        戻る
-      </Link>
-      <div className="border rounded-xl p-6 mt-4 flex flex-col gap-4">
-        <h1 className="font-bold text-center text-2xl">ぷらっとテスト</h1>
-        <div>
-          <Player autoplay loop src={BirdAnimation} className="w-40" />
-        </div>
-        <Button variant="primary" size="lg" onClick={onStart}>
-          スタート
-        </Button>
+    <div className="border rounded-xl p-6 mt-4 flex flex-col gap-4">
+      <h1 className="font-bold text-center text-2xl">ぷらっとテスト</h1>
+      <div>
+        <Player autoplay loop src={BirdAnimation} className="w-40" />
       </div>
-    </>
+      <Button variant="primary" size="lg" onClick={onStart}>
+        スタート
+      </Button>
+    </div>
   );
 };
 
