ebec7ea2366fdb1420f0ddbfb216de57560a40d8
Kazuma Ito <zarigani2x@gmail.com>
2024-12-02 08:32:17 +0900

    update styles for settings

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -3,6 +3,8 @@ import { auth, signOut } from "@/lib/auth";
 import PageHeading from "../PageHeading";
 import SettingsItem from "@/components/SettingsItem";
 import Image from "next/image";
+import Link from "next/link";
+import { ExternalLinkIcon } from "lucide-react";
 
 const Page = async () => {
   const session = await auth();
@@ -10,7 +12,7 @@ const Page = async () => {
   return (
     <>
       <PageHeading heading="設定" href="/admin/settings" />
-      <div className="mx-auto flex max-w-5xl flex-col gap-2 p-6">
+      <div className="mx-auto flex max-w-lg flex-col gap-4 p-6">
         <SettingsItem>
           <Image
             className="size-10 rounded-full"
@@ -19,7 +21,7 @@ const Page = async () => {
             width={80}
             height={80}
           />
-          <div className="mt-2 font-semibold">{session?.user?.name}</div>
+          <div className="font-semibold">{session?.user?.name}</div>
           <form
             className="ml-auto"
             action={async () => {
@@ -33,6 +35,28 @@ const Page = async () => {
               ログアウト
             </Button>
           </form>
+        </SettingsItem>
+        <SettingsItem>
+          <div className="text-sm font-semibold">サイト管理</div>
+          <Link
+            href="https://vercel.com/kyosan-cts/kyosan-cts"
+            className="ml-auto flex items-center gap-1.5 text-neutral-400 transition-colors hover:text-black"
+            target="_blank"
+          >
+            Vercel
+            <ExternalLinkIcon size={16} />
+          </Link>
+        </SettingsItem>
+        <SettingsItem>
+          <div className="text-sm font-semibold">データベース</div>
+          <Link
+            href="https://supabase.com/dashboard/project/lbefyeouwvwjuupbyilb"
+            className="ml-auto flex items-center gap-1.5 text-neutral-400 transition-colors hover:text-black"
+            target="_blank"
+          >
+            Supabase
+            <ExternalLinkIcon size={16} />
+          </Link>
         </SettingsItem>
       </div>
     </>
--- src/components/SettingsItem.tsx
+++ src/components/SettingsItem.tsx
@@ -3,7 +3,7 @@ type SettingsItemProps = {
 };
 
 const SettingsItem = ({ children }: SettingsItemProps) => {
-  return <div className="flex gap-4 rounded-xl border p-4">{children}</div>;
+  return <div className="flex items-center gap-4 rounded-xl border p-4">{children}</div>;
 };
 
 export default SettingsItem;
