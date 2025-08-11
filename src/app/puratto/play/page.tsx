bc3d0474ea6dd9e932e7a8efb9c3e93c60cc10b2
Kazuma Ito <zarigani2x@gmail.com>
2025-08-12 02:25:39 +0900

    revalidate on every request

--- src/app/puratto/play/page.tsx
+++ src/app/puratto/play/page.tsx
@@ -1,6 +1,8 @@
 import { getRandomQuizzes } from "@/lib/quiz-actions";
 import { ClientView } from "./client-view";
 
+export const revalidate = 0;
+
 const Page = async () => {
   const quizzes = await getRandomQuizzes(3);
   return <ClientView quizzes={quizzes} />;
