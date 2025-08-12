16ecf549c2c1695bce5afb149da6df3195c22121
Kazuma Ito <zarigani2x@gmail.com>
2025-08-12 12:05:47 +0900

    add preview overlay for mobile

--- src/components/admin/quiz/quiz-view.tsx
+++ src/components/admin/quiz/quiz-view.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { QuizData } from "@/lib/quiz-data";
 import { QuizValues } from "@/lib/quiz-editor";
-import { ArrowLeftIcon } from "lucide-react";
+import { ArrowLeftIcon, EyeIcon, XIcon } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 import { UseFormReturn } from "react-hook-form";
 import { QuizEditor } from "./quiz-editor";
 import { QuizPreview } from "./quiz-preview";
@@ -24,6 +25,8 @@ export const QuizView = ({
   labels,
   onSubmit,
 }: Props) => {
+  const [showPreviewOverlay, setShowPreviewOverlay] = useState(true);
+
   return (
     <div className="mx-auto flex h-[calc(100dvh-48px)] max-w-5xl">
       <div className="flex-1 overflow-auto">
@@ -35,6 +38,14 @@ export const QuizView = ({
               </Link>
             </Button>
             {heading && <div className="font-bold">{heading}</div>}
+            <Button
+              className="ml-auto sm:hidden"
+              variant="secondary"
+              onClick={() => setShowPreviewOverlay(true)}
+            >
+              <EyeIcon />
+              プレビュー
+            </Button>
           </div>
           <QuizEditor
             form={form}
@@ -44,9 +55,23 @@ export const QuizView = ({
           />
         </div>
       </div>
-      <div className="flex-1">
+      <div className="hidden flex-1 sm:block">
         <QuizPreview quiz={previewQuiz} />
       </div>
+      {showPreviewOverlay && (
+        <div className="bg-background fixed inset-0 flex flex-col transition-all duration-200 ease-out sm:hidden starting:scale-95 starting:opacity-0">
+          <div className="flex justify-end px-4 pt-4">
+            <Button
+              variant="secondary"
+              onClick={() => setShowPreviewOverlay(false)}
+            >
+              <XIcon />
+              閉じる
+            </Button>
+          </div>
+          <QuizPreview quiz={previewQuiz} />
+        </div>
+      )}
     </div>
   );
 };
