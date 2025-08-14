380689a49ae6280189087a20c0127c23dec89275
Kazuma Ito <zarigani2x@gmail.com>
2025-08-15 07:49:52 +0900

    update select form styles

--- src/components/quiz-form-select.tsx
+++ src/components/quiz-form-select.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SelectQuizData } from "@/lib/quiz-data";
+import { cn } from "@/lib/utils";
 import { shuffle } from "es-toolkit";
 import { useMemo } from "react";
 import { Button } from "./ui/button";
@@ -18,12 +19,12 @@ export const QuizFormSelect = ({
   showAnswer,
   setValue,
 }: Props) => {
+  const correctChoices = quiz.correctChoicesText?.split("\n") ?? [];
+  const incorrectChoices = quiz.incorrectChoicesText?.split("\n") ?? [];
+
   const choices = useMemo(() => {
     return shuffle(
-      [
-        ...(quiz.correctChoicesText?.split("\n") ?? []),
-        ...(quiz.incorrectChoicesText?.split("\n") ?? []),
-      ]
+      [...correctChoices, ...incorrectChoices]
         .map((choice) => choice.trim())
         .filter(Boolean),
     );
@@ -42,6 +43,7 @@ export const QuizFormSelect = ({
       <div className="flex flex-col gap-2">
         {choices.map((choice, i) => {
           const isSelected = value.includes(choice);
+          const isAnswer = correctChoices.includes(choice);
           return (
             <Button
               type="button"
@@ -50,6 +52,10 @@ export const QuizFormSelect = ({
               variant={isSelected ? "default" : "outline"}
               onClick={() => handleChoice(choice)}
               disabled={showAnswer}
+              className={cn("opacity-100!", {
+                "bg-red-500": showAnswer && !isAnswer && isSelected,
+                "bg-green-500!": showAnswer && isAnswer,
+              })}
             >
               {choice}
             </Button>
