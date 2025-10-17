f559097cca599639c76fe41f8179f4f98a04931f
Kazuma Ito <zarigani2x@gmail.com>
2025-10-17 14:39:25 +0900

    update home

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -5,15 +5,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col py-16">
-      <div className="flex flex-col items-center leading-18 tracking-[1rem] font-mono text-7xl font-medium sm:text-8xl">
-        <div className="">KYO</div>
-        <div className="">
-          S<Link href="admin">A</Link>N
-        </div>
-        <div className="">CTS</div>
+    <div className="flex flex-col">
+      <div className="my-20 text-center text-6xl font-semibold">
+        京産キャンスタ
       </div>
-      <div className="mt-20 flex justify-center gap-2">
+      <div className="flex justify-center gap-2">
         <Button variant="secondary" asChild>
           <Link href="/puratto">ぷらっとテスト</Link>
         </Button>
