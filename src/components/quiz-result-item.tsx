b2ad692cf3a04aad53bd6efaf9bf5a5aac354aaa
Kazuma Ito <zarigani2x@gmail.com>
2025-10-21 00:32:31 +0900

    update quiz result item

--- src/components/quiz-result-item.tsx
+++ src/components/quiz-result-item.tsx
@@ -1,6 +1,6 @@
 import { QuizResult } from "@/lib/quiz-form";
 import { QuizData } from "@/lib/quiz/data";
-import { CornerDownRightIcon, LightbulbIcon } from "lucide-react";
+import { CornerDownRightIcon } from "lucide-react";
 import { Markdown } from "./markdown";
 import { QuizAnswerRenderer } from "./quiz-answer-renderer";
 
@@ -17,27 +17,23 @@ export const QuizResultItem = ({ result, quiz, index }: Props) => {
         <div className="font-medium">第{index}問</div>
         <div className="font-bold">
           {result.isCorrect ? (
-            <span className="text-green-600">正解</span>
+            <span className="text-green-500">正解!!</span>
           ) : (
-            <span className="text-red-600">不正解</span>
+            <span className="text-rose-500">不正解</span>
           )}
         </div>
       </div>
-      <div className="bg-muted/50 -mx-6 mt-2 border p-1 shadow-lg sm:mx-0 sm:rounded-xl">
-        <div className="p-3">
+      <div className="bg-muted/50 -mx-6 mt-2 border p-1 sm:mx-0 sm:rounded-xl">
+        <div className="bg-background rounded-lg border px-4 py-3">
           <Markdown className="">{quiz.question}</Markdown>
         </div>
-        <div className="mb-3 flex gap-2 px-3">
-          <CornerDownRightIcon className="size-4 h-[1lh] shrink-0" />
+        <div className="my-4 flex gap-3 px-4">
+          <CornerDownRightIcon className="h-[1lh] w-4 shrink-0" />
           <QuizAnswerRenderer quiz={quiz} className="font-bold text-blue-500" />
         </div>
         {quiz.explanation && (
-          <div className="bg-background relative mt-3 rounded-lg border p-3">
-            <div className="flex items-center gap-1 text-sm font-semibold">
-              <LightbulbIcon className="size-3.5" />
-              解説
-            </div>
-            <Markdown className="mt-2 opacity-80">{quiz.explanation}</Markdown>
+          <div className="p-4 pt-0">
+            <Markdown className="">{quiz.explanation}</Markdown>
           </div>
         )}
       </div>
