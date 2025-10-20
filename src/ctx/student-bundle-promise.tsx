0f6f27b26f14b7ecfe908ce79d5a3807efd649ef
Kazuma Ito <zarigani2x@gmail.com>
2025-10-20 21:43:45 +0900

    refactor

--- src/ctx/student-bundle-promise.tsx
+++ src/ctx/student-bundle-promise.tsx
@@ -3,11 +3,17 @@
 import { StudentBundle } from "@/lib/students";
 import { createContext, useContext } from "react";
 
-export const StudentBundlePromiseContext =
+const StudentBundlePromiseContext =
   createContext<Promise<StudentBundle> | null>(null);
 
 export const useStudentBundlePromise = () => {
   const value = useContext(StudentBundlePromiseContext);
   if (!value) throw new Error("Missing StudentBundlePromiseContext");
   return value;
+};
+
+export const StudentBundlePromiseProvider = (
+  props: React.ComponentProps<typeof StudentBundlePromiseContext.Provider>,
+) => {
+  return <StudentBundlePromiseContext.Provider {...props} />;
 };
--- src/providers/student-bundle-promise-provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { StudentBundlePromiseContext } from "@/ctx/student-bundle-promise-context";
-
-export const StudentBundlePromiseProvider = (
-  props: React.ComponentProps<typeof StudentBundlePromiseContext.Provider>,
-) => {
-  return <StudentBundlePromiseContext.Provider {...props} />;
-};
