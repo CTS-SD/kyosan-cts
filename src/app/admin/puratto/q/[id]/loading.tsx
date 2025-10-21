972171d57fef434ba9c125caf6cb0e77df6efb0c
Kazuma Ito <zarigani2x@gmail.com>
2025-10-22 00:13:26 +0900

    use loading.tsx

---
+++ src/app/admin/puratto/q/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from "@/components/ui/spinner";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center py-40">
+      <Spinner />
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/admin/quiz/quiz-page-fallback.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
-export const QuizPageFallback = () => {
-  return (
-    <div className="mx-auto max-w-6xl p-6">
-      <Skeleton className="h-100 w-full mask-b-from-70%" />
-    </div>
-  );
-};
