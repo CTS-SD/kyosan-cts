9aa2e05b562cba5001c7ca57db4a6893f287f1f4
Kazuma Ito <zarigani2x@gmail.com>
2025-10-22 16:36:48 +0900

    style: update skeleton components with rounded corners for improved aesthetics

--- src/components/admin/dept/department-box-list-fallback.tsx
+++ src/components/admin/dept/department-box-list-fallback.tsx
@@ -2,9 +2,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export const DepartmentBoxListFallback = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 mask-b-from-60% sm:grid-cols-2">
+    <div className="grid grid-cols-1 gap-4 mask-b-to-40% sm:grid-cols-2">
       {Array.from({ length: 6 }).map((_, i) => (
-        <Skeleton key={i} className="h-60" />
+        <Skeleton key={i} className="h-60 rounded-xl" />
       ))}
     </div>
   );
--- src/components/admin/quiz/quiz-list-fallback.tsx
+++ src/components/admin/quiz/quiz-list-fallback.tsx
@@ -8,7 +8,7 @@ export const QuizListFallback = () => {
       </div>
       <div className="mt-4 grid grid-cols-1 gap-4 mask-b-to-60% sm:grid-cols-2 lg:grid-cols-3">
         {Array.from({ length: 9 }).map((_, index) => (
-          <Skeleton key={index} className="h-[184px]" />
+          <Skeleton key={index} className="h-[184px] rounded-xl" />
         ))}
       </div>
     </div>
