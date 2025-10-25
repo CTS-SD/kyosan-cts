1f025251b99a50a12d11901a2f489bf02b1a3c14
Kazuma Ito <zarigani2x@gmail.com>
2025-10-26 00:07:56 +0900

    refactor: update pnpm workspace dependencies and refactor QuizFormSelect component
    - Changed dependency format in pnpm-workspace.yaml to use single quotes for consistency.
    - Added 'unrs-resolver' to onlyBuiltDependencies in pnpm-workspace.yaml.
    - Replaced Button component with a native button element in QuizFormSelect for improved accessibility and performance.
    - Simplified button styling and removed unnecessary props related to the Button component.

--- src/components/quiz-form-select.tsx
+++ src/components/quiz-form-select.tsx
@@ -4,7 +4,6 @@ import { SelectQuizData } from "@/lib/quiz/data";
 import { cn } from "@/lib/utils";
 import { shuffle } from "es-toolkit";
 import { useMemo } from "react";
-import { Button } from "./ui/button";
 
 type Props = {
   quiz: SelectQuizData;
@@ -49,22 +48,19 @@ export const QuizFormSelect = ({
       <div className="flex flex-col gap-2">
         {choices.map((choice, i) => {
           const isSelected = value.includes(choice);
-          const isAnswer = correctChoices.includes(choice);
           return (
-            <Button
+            <button
               type="button"
               key={`${choice}-${i}`}
-              size="lg"
-              variant={isSelected ? "default" : "outline"}
               onClick={() => handleChoice(choice)}
               disabled={showAnswer}
-              className={cn("opacity-100!", {
-                "bg-red-500": showAnswer && !isAnswer && isSelected,
-                "bg-green-500!": showAnswer && isAnswer,
-              })}
+              className={cn(
+                "flex justify-center rounded-lg leading-tight border border-b-3 px-3 py-2.5 text-start font-semibold transition-all duration-50 active:mb-0.5 active:translate-y-0.5 active:border-b",
+                isSelected && "border-blue-300 bg-blue-100/60",
+              )}
             >
               {choice}
-            </Button>
+            </button>
           );
         })}
       </div>
