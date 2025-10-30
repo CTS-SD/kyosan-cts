9b6f00ed9adde74266472f277c859b052289d9a3
Kazuma Ito <zarigani2x@gmail.com>
2025-10-30 18:00:26 +0900

    fix: add custom font class to AppHeader link

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "./user-button";
 export const AppHeader = () => {
   return (
     <div className="mx-auto flex h-12 max-w-5xl items-center px-6">
-      <Link href="/" className="text-sm font-semibold">
+      <Link href="/" className="text-sm font-semibold font-zmg">
         京産キャンスタ
       </Link>
       <div className="ml-auto flex items-center gap-2">
