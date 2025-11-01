8886e31e087d909d7931d7818a97ca5b4da00531
Kazuma Ito <zarigani2x@gmail.com>
2025-11-02 08:51:35 +0900

    refactor: destructure quizzes from getQuizzes response for clarity

--- src/app/puratto/play/page.tsx
+++ src/app/puratto/play/page.tsx
@@ -4,8 +4,9 @@ import { ClientView } from "./client-view";
 export const revalidate = 0;
 
 const Page = async () => {
-  const quizzes = await getQuizzes({
+  const { quizzes } = await getQuizzes({
     limit: 5,
+    offset: 0,
     orderBy: "random",
   });
   return <ClientView quizzes={quizzes} />;
