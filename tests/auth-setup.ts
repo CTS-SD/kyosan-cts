7592d4b3e7dc09eb56d4ff26336c8e3b3da1b787
Kazuma Ito <zarigani2x@gmail.com>
2025-11-09 08:07:15 +0900

    add error handling

--- tests/auth-setup.ts
+++ tests/auth-setup.ts
@@ -1,7 +1,12 @@
 import { test as setup } from "@playwright/test";
 
 setup("authenticate", async ({ page }) => {
-  await page.goto("/api/auth/test-login");
+  const res = await page.goto("/api/auth/test-login");
+
+  if (res?.status() !== 200) {
+    throw new Error("Failed to authenticate");
+  }
+
   await page.context().storageState({
     path: "playwright/.auth/user.json",
   });
