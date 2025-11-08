64b02b80a7183ea183319168eb8e98d30ba0cfcd
Kazuma Ito <zarigani2x@gmail.com>
2025-11-09 00:52:58 +0900

    add useQuizzes

--- src/components/admin/quiz/quiz-list.tsx
+++ src/components/admin/quiz/quiz-list.tsx
@@ -2,37 +2,29 @@
 
 import { Alert, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import { getQuizzes } from "@/lib/quiz/actions";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useQuizzes } from "@/hooks/use-quizzes";
 import { AlertCircleIcon } from "lucide-react";
 import { QuizItem } from "./quiz-item";
 import { QuizListSkeleton } from "./quiz-list-skeleton";
 
-const PAGE_SIZE = 24;
-
 export const QuizList = () => {
-  const q = useInfiniteQuery({
-    queryKey: ["quizzes"],
-    initialPageParam: 0,
-    queryFn: async ({ pageParam }) =>
-      getQuizzes({
-        limit: PAGE_SIZE,
-        offset: pageParam,
-      }),
-    getNextPageParam: (last) => (last.hasMore ? last.nextCursor : undefined),
-    staleTime: 1000 * 60 * 5,
-  });
+  const {
+    quizzes,
+    isLoading,
+    isFetchingNextPage,
+    isError,
+    hasNextPage,
+    fetchNextPage,
+  } = useQuizzes();
 
-  if (q.isLoading && !q.data) return <QuizListSkeleton />;
-  if (q.isError)
+  if (isLoading) return <QuizListSkeleton />;
+  if (isError)
     return (
       <Alert variant="destructive">
         <AlertCircleIcon />
         <AlertTitle>問題の取得中にエラーが発生しました。</AlertTitle>
       </Alert>
     );
-
-  const quizzes = q.data!.pages.flatMap((p) => p.quizzes);
 
   return (
     <div>
@@ -41,15 +33,15 @@ export const QuizList = () => {
           <QuizItem key={quiz.id} quiz={quiz} />
         ))}
       </div>
-      {q.hasNextPage ? (
+      {hasNextPage ? (
         <div className="mt-4">
           <Button
             className="w-full"
             variant="outline"
-            onClick={() => q.fetchNextPage()}
-            disabled={q.isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
           >
-            {q.isFetchingNextPage ? "読み込み中..." : "さらに表示"}
+            {isFetchingNextPage ? "読み込み中..." : "さらに表示"}
           </Button>
         </div>
       ) : null}
---
+++ src/hooks/use-quizzes.ts
@@ -0,0 +1,25 @@
+import { getQuizzes } from "@/lib/quiz/actions";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+const PAGE_SIZE = 24;
+
+export const useQuizzes = () => {
+  const query = useInfiniteQuery({
+    queryKey: ["quizzes"],
+    initialPageParam: 0,
+    queryFn: async ({ pageParam }) =>
+      getQuizzes({
+        limit: PAGE_SIZE,
+        offset: pageParam,
+      }),
+    getNextPageParam: (last) => (last.hasMore ? last.nextCursor : undefined),
+    staleTime: 1000 * 60 * 5,
+  });
+
+  const quizzes = query.data?.pages.flatMap((p) => p.quizzes) ?? [];
+
+  return {
+    quizzes,
+    ...query,
+  };
+};
