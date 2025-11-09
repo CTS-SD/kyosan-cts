500b42d99c75a059dc3ef38f66ed19542a08ebc9
Kazuma Ito <zarigani2x@gmail.com>
2025-11-09 09:57:52 +0900

    add db seed

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,10 @@
+import { config } from "dotenv";
 import { cleanEnv, str, url } from "envalid";
 
+config();
+
 export const env = cleanEnv(process.env, {
+  DATABASE_URL: str(),
   BETTER_AUTH_SECRET: str(),
   BETTER_AUTH_URL: url(),
   GOOGLE_CLIENT_ID: str(),
