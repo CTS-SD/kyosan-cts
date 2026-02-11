e23d233756d68a7952d581673b81757c45122f14
Kazuma Ito <zarigani2x@gmail.com>
2026-02-11 15:01:43 +0900

    feat: enhance Loading component with UserAvatar and improved loading message layout

--- app/(public)/puratto/play/loading.tsx
+++ app/(public)/puratto/play/loading.tsx
@@ -1,5 +1,14 @@
+import { UserAvatar } from "@/components/user-avatar";
+
 const Loading = () => {
-  return <div className="grid h-dvh animate-pulse place-content-center">読み込み中...</div>;
+  return (
+    <div className="grid h-dvh place-content-center">
+      <div className="flex items-center gap-4">
+        <UserAvatar />
+        <div className="animate-pulse font-accent font-medium">ぷらっとテストを準備中...</div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
