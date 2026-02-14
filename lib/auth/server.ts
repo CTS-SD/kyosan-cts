21156996191dc28addbad0fb1850dd972e5ce143
Kazuma Ito <zarigani2x@gmail.com>
2026-02-15 02:50:08 +0900

    fix: remove nextCookies plugin from betterAuth configuration

--- lib/auth/server.ts
+++ lib/auth/server.ts
@@ -1,6 +1,5 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { nextCookies } from "better-auth/next-js";
 import { db } from "../db";
 import * as schema from "../db/schema";
 import { env } from "../env";
@@ -35,5 +34,4 @@ export const auth = betterAuth({
       },
     },
   },
-  plugins: [nextCookies()],
 });
